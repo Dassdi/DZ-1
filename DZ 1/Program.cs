ba117ab3718b52fbf9b3c5964b02698e85d243a0
Diana <dassdin@gmail.com>
2022-08-31 19:59:25 +0300

    dz1

---
+++ DZ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿int a,b;
+string input_a, input_b;
+
+Console.WriteLine("Введите два числа: ");
+
+input_a = Console.ReadLine();
+input_b = Console.ReadLine();
+
+a = Int32.Parse(input_a);
+b = Int32.Parse(input_b);
+
+if (a>b)
+{ 
+Console.WriteLine("Наибольшее число: " + a);
+}
+else
+{
+Console.WriteLine("Наибольшее число: " + b);
+}
