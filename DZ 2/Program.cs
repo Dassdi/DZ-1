43c36c52582c8188379d7c92437703c21893f6b0
Diana <dassdin@gmail.com>
2022-08-31 22:35:57 +0300

    dz2

--- DZ 2/Program.cs
+++ DZ 2/Program.cs
@@ -1,20 +1,30 @@
-﻿int a,b;
-string input_a, input_b;
+﻿int num1,num2,num3;
+string input_1, input_2, input_3;
+int max;
 
-Console.WriteLine("Введите два числа: ");
+Console.WriteLine("Введите три числа: ");
 
-input_a = Console.ReadLine();
-input_b = Console.ReadLine();
+input_1 = Console.ReadLine();
+input_2 = Console.ReadLine();
+input_3 = Console.ReadLine();
 
-a = Int32.Parse(input_a);
-b = Int32.Parse(input_b);
+num1 = Int32.Parse(input_1);
+num2 = Int32.Parse(input_2);
+num3 = Int32.Parse(input_3);
 
-if (a>b)
+max=num1;
+
+if (max < num2)
 { 
-Console.WriteLine("Наибольшее число: " + a);
-
+max = num2;
+ if (max < num3)
+  {
+    max = num3;
+  }
 }
 else
 {
-Console.WriteLine("Наибольшее число: " + b);
+    max = num1;
 }
+
+Console.WriteLine ("Наибольшее число: " + max);
