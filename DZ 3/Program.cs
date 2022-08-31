60df4cdf06e99cb9f371b421955f6847984716a0
Diana <dassdin@gmail.com>
2022-08-31 22:43:38 +0300

    dz3

---
+++ DZ 3/Program.cs
@@ -0,0 +1,17 @@
+﻿int a;
+string input_a;
+
+Console.WriteLine("Введите число: ");
+
+input_a = Console.ReadLine();
+a = Int32.Parse(input_a);
+
+if (a%2 == 0)
+{ 
+Console.WriteLine("Четное число");
+}
+else
+{
+Console.WriteLine("Нечетное число");
+}
+
