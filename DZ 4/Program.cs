8a954155a9de2d37de6764a2846a936112090a68
Diana <dassdin@gmail.com>
2022-08-31 23:04:13 +0300

    dz4

---
+++ DZ 4/Program.cs
@@ -0,0 +1,17 @@
+﻿int a,x;
+string input_a;
+string input_x;
+
+Console.Write("Введите число: ");
+
+input_a = Console.ReadLine();
+input_x = Console.ReadLine();
+a = Int32.Parse(input_a);
+x = Int32.Parse(input_a);
+
+for (x=1; x<a; x++)
+if (x%2 == 0)
+{ 
+Console.WriteLine(x);
+}
+
